9f61e2322c121ed0a9d2b0e9ac23bddbbecbda11
Mustafa Kemal DEMİR <41470942+mustafakemaldemir9@users.noreply.github.com>
2021-04-24 16:43:45 +0300

    Last Update

--- Models/TeacherDeleteModel.cs
+++ Models/TeacherDeleteModel.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace online_education_site.Models
 {
     public class TeacherDeleteModel
     {
+        [Required(ErrorMessage = "E-mail field is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Display(Name = "Email")]
         public string user_Email { get; set; }
+
+        [Required(ErrorMessage = "Password field is required.")]
+        [DataType(DataType.Password)]
+        [MinLength(6)]
+        [Display(Name = "Password")]
         public string user_Password { get; set; }
     }
 }
